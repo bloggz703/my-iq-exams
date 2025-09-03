83fa2fca0a8b9e94cf6b162bf82ed13757814cbd
bloggz703 <bloggz703@gmail.com>
2025-09-03 21:53:49 +0100

    new ads changes

--- src/AdsterraAd.tsx
+++ src/AdsterraAd.tsx
@@ -7,14 +7,7 @@ interface AdProps {
 
 export default function AdsterraAd({ step }: AdProps) {
   useEffect(() => {
-    const container = document.getElementById(
-      "container-f1c21c47e6a3d0697469f51f34e31c53"
-    );
-    if (container) container.innerHTML = "";
-
-    const oldScript = document.getElementById("adsterra-script");
-    if (oldScript) oldScript.remove();
-
+    
     const script = document.createElement("script");
     script.id = "adsterra-script";
     script.async = true;
@@ -22,18 +15,25 @@ export default function AdsterraAd({ step }: AdProps) {
     script.src =
       "//pl26687007.profitableratecpm.com/f1c21c47e6a3d0697469f51f34e31c53/invoke.js";
 
-    document.body.appendChild(script);
+    const container = document.getElementById(
+      "container-f1c21c47e6a3d0697469f51f34e31c53"
+    );
+    if (container) {
+      container.innerHTML = ""; 
+      container.appendChild(script);
+    }
   }, [step]);
 
   return (
     <AnimatePresence mode="wait">
       <motion.div
-        key={step} // 
+        key={step}
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
         exit={{ opacity: 0 }}
         transition={{ duration: 0.5 }}
       >
+        {/* keep this div persistent across steps */}
         <div id="container-f1c21c47e6a3d0697469f51f34e31c53"></div>
       </motion.div>
     </AnimatePresence>
